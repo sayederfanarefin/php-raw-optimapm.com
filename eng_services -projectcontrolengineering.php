a192c0ca6845f2a885939a0aaf2e38795a2d36b8
unknown <erfanjordison@gmail.com>
2018-03-21 02:54:05 +0600

    re organizing pages

--- eng_services -projectcontrolengineering.php
+++ eng_services -projectcontrolengineering.php
@@ -33,131 +33,129 @@
 	<!--[if lt IE 9]>
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-		<![endif]-->
-	</head>
+	<![endif]-->
+</head>
 
-	<body>
+<body>
 
-		<!-- Header -->
-		<header>
+	<!-- Header -->
+	<header>
 
-			<?php include 'nav.php';?>
+		<?php include 'nav.php';?>
 
-			<!-- header wrapper -->
-			<div class="bg-grey">
-				<div class="container abc" style="padding-left: 50px;padding-right: 50px;padding-bottom: 50px;">
-
-
-
-					<div class="section-header text-center" >
-						<h2 class="title" style="padding-top: 10vh;">Project Control Engineering</h2>
-					</div>
+		<!-- header wrapper -->
+		<div class="bg-grey">
+			<div class="container abc" style="padding-left: 50px;padding-right: 50px;padding-bottom: 50px;">
 
 
 
-				</div>
-			</div>
-			</header>
-			<!-- /header wrapper -->
-
-
-
-			<!-- right -->
-
-<div class="row"  style="padding-top: 5vh;>
-				<div class="col-md-1"></div>
-
-
-
-
-				<div class="col-md-10">
-
-					<div class="row abc" >
-
-						<div class="col-md-6">
-
-							<div class="team" data-toggle="modal" data-target="#exampleModal">
-								<div class="team-img">
-									<img class="img-responsive" src="./img/projectcontrols_hd.jpg" alt="">
-
-								</div>
-
-
-							</div>
-
-
-							
-						</div>
-
-						<div class="col-md-6">
-
-<p>Optima works with clients to develop, implement and maintain risk, schedule and cost controls on projects to optimize their performance. Our consultants specialize in centralized project control system for municipal and private site development including: feasibility studies, estimating, planning, waste water collection, water distribution, storm water management, roads, paving, grading, land division, surveying, construction layout, and GIS services.
-
-In working with our clients, we have developed a reputation for providing a high level of quality and reliability.  Numerous repeat clients and referrals by other professionals and regulatory agencies have been the results of our proven efficiency and effectiveness
-
-Our consultants strive to keep abreast of advances in engineering and management techniques, utilizing the latest thinking and technology.</p>
-
-						</div>
-						
-					</div>
-
-
-
-
+				<div class="section-header text-center" >
+					<h2 class="title" style="padding-top: 10vh;">Project Control Engineering</h2>
 				</div>
 
 
-				<div class="col-md-1"></div>
+
+			</div>
+		</div>
+	</header>
+	<!-- /header wrapper -->
+
+
+
+	<!-- right -->
+
+	<div class="row"  style="padding-top: 5vh;">
+
+		<div class="col-md-1">
+
+
+		</div>
+
+
+
+
+		<div class="col-md-10">
+
+			<div class="row abc" >
+				<div class="col-md-2">
+				</div>
+				<div class="col-md-8">
+					<img class="img-responsive" src="./img/projectcontrols_hd.jpg" alt="">
+
+				</div>
 			</div>
 
-<!-- eight end-->
+			<div class="row abc" >
+
+				<div class="col-md-12">
+
+					<p><br><br>Optima works with clients to develop, implement and maintain risk, schedule and cost controls on projects to optimize their performance. Our consultants specialize in centralized project control system for municipal and private site development including: feasibility studies, estimating, planning, waste water collection, water distribution, storm water management, roads, paving, grading, land division, surveying, construction layout, and GIS services.
+
+						In working with our clients, we have developed a reputation for providing a high level of quality and reliability.  Numerous repeat clients and referrals by other professionals and regulatory agencies have been the results of our proven efficiency and effectiveness
+
+					Our consultants strive to keep abreast of advances in engineering and management techniques, utilizing the latest thinking and technology.<br></p>
+
+				</div>
+
+			</div>
 
 
 
+
+		</div>
+
+
+		<div class="col-md-1"></div>
 	</div>
 
+	<!-- eight end-->
 
-		
-		<!-- /Header -->
-<div class="row">
-<br><br><br>
+
+
 </div>
-		
 
-	<?php include 'footer.php';?>
 
-				<!-- Back to top -->
-				<div id="back-to-top"></div>
-				<!-- /Back to top -->
 
-				<!-- Preloader -->
-				<div id="preloader">
-					<div class="preloader">
-						<span></span>
-						<span></span>
-						<span></span>
-						<span></span>
-					</div>
-				</div>
-				<!-- /Preloader -->
+<!-- /Header -->
+<div class="row">
+	<br><br><br>
+</div>
 
-				<!-- jQuery Plugins -->
-				<script type="text/javascript" src="js/jquery.min.js"></script>
-				<script type="text/javascript" src="js/bootstrap.min.js"></script>
-				<script type="text/javascript" src="js/owl.carousel.min.js"></script>
-				<script type="text/javascript" src="js/jquery.magnific-popup.js"></script>
-				<script type="text/javascript" src="js/main.js"></script>
 
-				<style type="text/css">
+<?php include 'footer.php';?>
+
+<!-- Back to top -->
+<div id="back-to-top"></div>
+<!-- /Back to top -->
+
+<!-- Preloader -->
+<div id="preloader">
+	<div class="preloader">
+		<span></span>
+		<span></span>
+		<span></span>
+		<span></span>
+	</div>
+</div>
+<!-- /Preloader -->
+
+<!-- jQuery Plugins -->
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/owl.carousel.min.js"></script>
+<script type="text/javascript" src="js/jquery.magnific-popup.js"></script>
+<script type="text/javascript" src="js/main.js"></script>
+
+<style type="text/css">
 
 
 
 
 .abc li:before {
-  content: "•";
-  padding-right: 5px;
-  }
-  </style>
+	content: "•";
+	padding-right: 5px;
+}
+</style>
 </body>
 
 </html>
