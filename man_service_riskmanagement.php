d4fc7ca3450f29a738cfe270669cb44312adb1c7
unknown <erfanjordison@gmail.com>
2018-03-21 10:00:15 +0600

    abc

--- man_service_riskmanagement.php
+++ man_service_riskmanagement.php
@@ -72,18 +72,46 @@
 				<div class="col-md-10">
 
 					<div class="row abc" >
+
+						<div class="col-md-5">
+									<img class="img-responsive" src="./img/disruption_mainart2.jpg" alt="">
+								
+						</div>
+
+
 						<div class="col-md-6">
 
+							<p><br>Risk management is the way toward recognizing, surveying and controlling threats to an organization. These dangers, or risks, could come from a wide assortment of sources, financial uncertainty, legal liabilities, strategic management errors, accidents and natural disasters. Recently internet security threats and data-related risks, and the risk management strategies to alleviate them, have become a top priority.</p>
 
-							<!-- <h3><strong><u>Construction Management</u></strong></h3> -->
-							<p>Risk management is the way toward recognizing, surveying and controlling threats to an organization. These dangers, or risks, could come from a wide assortment of sources, financial uncertainty, legal liabilities, strategic management errors, accidents and natural disasters. Recently internet security threats and data-related risks, and the risk management strategies to alleviate them, have become a top priority.</p>
-<h3>All r<strong>isk management plans follow these </strong>steps that combine to make up the overall risk management process:</h3>
+
+</div>
+						
+					</div>
+
+						<div class="row abc" >
+
+						<div class="col-md-12">
+
+
+
+<h5><br><br>All r<strong>isk management plans follow these </strong>steps that combine to make up the overall risk management process:</h5>
 <ul>
-<li><strong><u>Risk identification:</u></strong>The Company identifies and defines potential risks that may negatively influence a specific company process or project.</li>
-<li><strong><u>Risk analysis:</u></strong>Once specific types of risk are identified, the company then determines the odds of it occurring, as well as its consequences. The goal of the analysis is to further understand each specific instance of risk, and how it could influence the company&#8217;s projects and objectives.</li>
-<li><a href="http://searchcompliance.techtarget.com/definition/risk-assessment"><strong>Risk assessment</strong></a><strong><u> and evaluation:</u></strong>The risk is then further evaluated after determining the risk&#8217;s overall likelihood of occurrence combined with its overall consequence.<em> </em>The company can then make decisions on whether the risk is acceptable and whether the company is willing to take it on based on its <a href="http://searchcompliance.techtarget.com/definition/risk-appetite">risk appetite</a>.</li>
-<li><strong><u>Risk mitigation</u></strong><em>: </em>During this step, companies assess their highest-ranked risks and develop a plan to alleviate them using specific risk controls. These plans include risk mitigation processes, risk prevention tactics and contingency plans in the event the risk comes to fruition.</li>
-<li><strong><u>Risk monitoring</u></strong><strong><em><u>:</u></em></strong>Part of the mitigation plan includes following up on both the risks and the overall plan to continuously monitor and track new and existing risks. The overall risk management process should also be reviewed and updated accordingly.</li>
+
+
+<h5>Risk identification:</h5>The Company identifies and defines potential risks that may negatively influence a specific company process or project.
+
+
+<br><br>
+<h5>Risk analysis:</h5>Once specific types of risk are identified, the company then determines the odds of it occurring, as well as its consequences. The goal of the analysis is to further understand each specific instance of risk, and how it could influence the company&#8217;s projects and objectives.
+
+<br><br>
+<h5>Risk assessment and evaluation:</h5>The risk is then further evaluated after determining the risk&#8217;s overall likelihood of occurrence combined with its overall consequence.<em> </em>The company can then make decisions on whether the risk is acceptable and whether the company is willing to take it on based on its <a href="http://searchcompliance.techtarget.com/definition/risk-appetite">risk appetite</a>.<br>
+<br>
+<h5>Risk mitigation:</h5>During this step, companies assess their highest-ranked risks and develop a plan to alleviate them using specific risk controls. These plans include risk mitigation processes, risk prevention tactics and contingency plans in the event the risk comes to fruition.
+
+<br>
+<br>
+<h5>Risk monitoring: </h5>Part of the mitigation plan includes following up on both the risks and the overall plan to continuously monitor and track new and existing risks. The overall risk management process should also be reviewed and updated accordingly.
 </ul>
 <p>&nbsp;</p>
 <p><strong><u>Risk management approaches</u></strong></p>
@@ -101,20 +129,7 @@
 
 
 						</div>
-						<div class="col-md-6">
-
-							<div class="team" data-toggle="modal" data-target="#exampleModal">
-								<div class="team-img">
-									<img class="img-responsive" src="./img/disruption_mainart2.jpg" alt="">
-
-								</div>
-
-
-							</div>
-
-
-							
-						</div>
+						
 					</div>
 
 
