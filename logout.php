7d1007e9239166437aa9cec032d9e1caf46916e9
System Administrator <root@Tasnims-MacBook-Pro.local>
2018-03-25 11:53:21 +0600

    fucked news

--- logout.php
+++ logout.php
@@ -5,6 +5,6 @@ session_start();?>
 
 session_destroy();
 
-header('Location: index');
+header('Location: index.php');
 
 ?>
--- yo/includes/connection.php
+++
@@ -1,3 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost","root","sXdG16000000_6","newsbuzz" ) or die ("error" . mysqli_error($conn));
-?>
