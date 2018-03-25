8b661c5cb555357ce6569e157bb5de14e806e6be
System Administrator <root@Tasnims-MacBook-Pro.local>
2018-03-25 12:06:41 +0600

    fucked news

--- edit/includes/adminnav.php
+++ edit/includes/adminnav.php
@@ -8,7 +8,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index">NEWS-BUZZ ADMIN PANEL</a>
+                <a class="navbar-brand" href="">ADMIN PANEL</a>
             </div>
             
             <ul class="nav navbar-right top-nav">
