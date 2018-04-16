0414219f68586f76e2280db9ead7e72abdd73b4d
unknown <erfanjordison@gmail.com>
2018-04-16 12:54:29 +0600

    dibe

--- includes/connection.php
+++ includes/connection.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost","root","","newsbuzz" ) or die ("error" . mysqli_error($conn));
+$conn = mysqli_connect("localhost","root","sXdG16000000_6","newsbuzz" ) or die ("error" . mysqli_error($conn));
 
 //azure, password | root, sXdG16000000_6
 ?>
